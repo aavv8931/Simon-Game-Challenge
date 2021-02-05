e3e00c9403c8a4e51400a126eab9df1388875059
Arturo <arturo_veintimilla@hotmail.com>
2021-02-05 00:41:04 -0500

    Add sounds

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
  var buttonColours = ["red","blue","green","yellow"];
 
- var gamePattern = []
+ var gamePattern = [];
 
 function nextSequence (){
-   var randomNumber = Math.floor(Math.random() * 3) + 1;
+   var randomNumber = Math.floor(Math.random() * 4);
 
-   return randomNumber;
+   var randomChosenColour = buttonColours[randomNumber];
+
+   gamePattern.push(randomChosenColour);
+
+   $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+ var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+ audio.play();
+
 } 
 
-var randomChosenColour = buttonColours[randomNumber];
-
-gamePattern.push(randomChosenColour)
